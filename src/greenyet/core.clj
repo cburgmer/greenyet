8125f77c385e56ae3b5bb60a14fc97853b60ec12
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-05-16 22:15:13 +0800

    Handle multiple systems

--- src/greenyet/core.clj
+++ src/greenyet/core.clj
@@ -2,21 +2,40 @@
   (:require [hiccup.core :refer [html]]))
 
 (def dummy-host-list [{:host "abcd1234" :environment "DEV" :system "MySystem"}
-                      {:host "xyz123" :environment "PROD" :system "MySystem"}])
+                      {:host "xyz123" :environment "PROD" :system "MySystem"}
+                      {:host "fgh456" :environment "DEV" :system "AnotherSystem"}])
 
 
-(defn- environment-entry [host-list environment]
+(defn- host-for-environment [host-list environment]
   (first (filter #(= environment (:environment %)) host-list)))
+
+(defn- system-row [environments host-list]
+  (map (fn [environment]
+         (host-for-environment host-list environment))
+       environments))
+
+(defn- environment-table [environments host-list]
+  (->> host-list
+       (group-by :system)
+       (map (fn [[_ system-host-list]]
+              (system-row environments system-host-list)))))
+
+
+(defn- environment-table-as-html [environments rows]
+  (html [:table
+         [:thead
+          [:tr
+           (cons [:td]
+                 (map (fn [env] [:td env]) environments))]]
+         [:tbody
+          (map (fn [row]
+                 [:tr
+                  (cons [:td (:system (first row))]
+                        (map (fn [host] [:td (:host host)]) row))])
+               rows)]]))
+
 
 (defn handler [x]
   (let [environments (distinct (map :environment dummy-host-list))
-        row (map (partial environment-entry dummy-host-list) environments)]
-    {:body (html [:table
-                  [:thead
-                   [:tr
-                    (cons [:td]
-                          (map (fn [env] [:td env]) environments))]]
-                  [:tbody
-                   [:tr
-                    (cons [:td (:system (first dummy-host-list))]
-                          (map (fn [host] [:td (:host host)]) row))]]])}))
+        rows (environment-table environments dummy-host-list)]
+    {:body (environment-table-as-html environments rows)}))
