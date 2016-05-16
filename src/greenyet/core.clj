fd37e3719a76fbcaac70a7d81d6fca0e5e2fe0ab
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-05-16 22:32:38 +0800

    Display colors

--- src/greenyet/core.clj
+++ src/greenyet/core.clj
@@ -1,9 +1,12 @@
 (ns greenyet.core
-  (:require [hiccup.core :refer [html]]))
+  (:require [hiccup.core :refer [html]]
+            [clojure.string :as str]))
 
 (def dummy-host-list [{:host "abcd1234" :environment "DEV" :system "MySystem"}
                       {:host "xyz123" :environment "PROD" :system "MySystem"}
                       {:host "fgh456" :environment "DEV" :system "AnotherSystem"}])
+
+(def host-list-with-status (map #(assoc % :color :yellow) dummy-host-list))
 
 
 (defn- host-for-environment [host-list environment]
@@ -21,21 +24,31 @@
               (system-row environments system-host-list)))))
 
 
+(defn- host-as-html [host]
+  [:td {:class (:color host)}
+   (:host host)])
+
 (defn- environment-table-as-html [environments rows]
-  (html [:table
-         [:thead
-          [:tr
-           (cons [:td]
-                 (map (fn [env] [:td env]) environments))]]
-         [:tbody
-          (map (fn [row]
-                 [:tr
-                  (cons [:td (:system (first row))]
-                        (map (fn [host] [:td (:host host)]) row))])
-               rows)]]))
+  (html [:head
+         [:style (str/join "\n" [".green { background-color: green; }"
+                                 ".yellow { background-color: yellow; }"
+                                 ".red { background-color: red; }"])]]
+        [:body
+         [:table
+          [:thead
+           [:tr
+            [:td]
+            (for [env environments]
+              [:td env])]]
+          [:tbody
+           (for [row rows]
+             [:tr
+              [:td (:system (first row))]
+              (for [host row]
+                (host-as-html host))])]]]))
 
 
 (defn handler [x]
-  (let [environments (distinct (map :environment dummy-host-list))
-        rows (environment-table environments dummy-host-list)]
+  (let [environments (distinct (map :environment host-list-with-status))
+        rows (environment-table environments host-list-with-status)]
     {:body (environment-table-as-html environments rows)}))
