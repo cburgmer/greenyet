cbab4ea1f8e945427cccbd6a685b0137f1020978
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-06-01 22:28:40 +0800

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject greenyet "0.1.2"
+(defproject greenyet "0.2.0"
   :description "Are my machines green yet?"
   :url "https://github.com/cburgmer/greenyet"
   :license {:name "BSD 2-Clause"
