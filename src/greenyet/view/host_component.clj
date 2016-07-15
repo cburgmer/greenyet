6ef481789eac2a38d2596bc12cc656c60706a64b
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-07-16 00:07:26 +0800

    Remove legacy code

--- src/greenyet/view/host_component.clj
+++ src/greenyet/view/host_component.clj
@@ -8,46 +8,33 @@
     (str/join ", " message)
     message))
 
+(defn- status-symbol [{color :color}]
+  [:span.symbol {:class (some-> color
+                                name
+                                h)}])
+
 (def ^:private color-by-importance [:red :yellow :green])
 
 (defn render [host status]
-  [:li.patch.status-patch {:class (str/join " " [(some-> status
-                                                         :color
-                                                         name
-                                                         h)])}
+  [:li.patch.status-patch {:class (some-> status
+                                          :color
+                                          name
+                                          h)}
    [:span.system [:a {:href (str/join ["?systems=" (h (:system host))])}
                   (h (:system host))]]
    [:span.environment (h (:environment host))]
-   [:span.state [:span.symbol {:class (some-> status
-                                              :color
-                                              name
-                                              h)}]]
+   [:span.state (status-symbol status)]
 
    [:span.detail (h (or (message status) "No details to report"))
     (when (:components status)
-      (let [components-id (h (str/join ["components-" (:index host)]))]
+      [:ol.joblist
+       (for [comp (utils/prefer-order-of color-by-importance (:components status) :color)]
+         [:li.job {:title (h (message comp))}
 
-        [:ol.joblist {:id components-id }
-         (for [comp (utils/prefer-order-of color-by-importance (:components status) :color)]
-           [:li.job {:class (str/join " " [(some-> comp
-                                                   :color
-                                                   name
-                                                   h)])
-                     :title (h (message comp))
-                     :data-name (h (:name comp))}
-
-            [:span.jobstatus.symbol {:class (some-> comp
-                                                    :color
-                                                    name
-                                                    h)}]
-            (h (:name comp))])
-         ]))
-    ]
+          (status-symbol comp)
+          (h (:name comp))])])]
 
    (when (:package-version status)
      [:span.meta-data (h (:package-version status))])
    [:span.url [:a {:href (h (:status-url host))}
-               (h (:hostname host))]]
-
-
-   ])
+               (h (:hostname host))]]])
