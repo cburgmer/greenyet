26be725ada36f4111ae9629b6a25a2908816ffab
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-10-28 23:13:17 +0800

    Refactor, pull out handler, separate setup from routing/rendering

--- src/greenyet/core.clj
+++ src/greenyet/core.clj
@@ -1,81 +1,10 @@
 (ns greenyet.core
-  (:require [clj-yaml.core :as yaml]
-            [cheshire.core :as j]
-            [clojure
-             [string :as str]
-             [walk :refer [keywordize-keys]]]
-            [clojure.java.io :as io]
+  (:require [clojure.string :as str]
             [greenyet
              [config :as config]
-             [poll :as poll]
-             [selection :as selection]
-             [utils :as utils]]
-            [greenyet.view
-             [patchwork :as patchwork]
-             [styleguide :as styleguide]]
-            [ring.middleware
-             [content-type :as content-type]
-             [not-modified :as not-modified]
-             [params :as params]
-             [resource :as resource]]
-            [ring.util
-             [response :refer [header]]
-             [time :refer [format-date]]])
+             [handler :as handler]
+             [poll :as poll]])
   (:import java.io.FileNotFoundException))
-
-(defn- page-template []
-  (-> "index.template.html" io/resource slurp))
-
-(defn- styleguide-template []
-  (-> "styleguide.template.html" io/resource slurp))
-
-(defn- environment-names []
-  (-> "environment_names.yaml" io/resource slurp yaml/parse-string))
-
-
-(defn cache-headers [response last-changed]
-  (-> response
-      (header "Last-Modified" (format-date (.toDate last-changed)))
-      (header "Cache-Control" "max-age=0,must-revalidate")))
-
-(defn- host-with-statuses [params]
-  (let [[host-with-statuses _] @poll/statuses
-        hide-green (= (get params "hideGreen") "true")]
-    (selection/filter-hosts host-with-statuses
-                            (get params "systems")
-                            (get params "environments")
-                            hide-green)))
-
-(defn- render-environments [params]
-  (let [[_ last-changed] @poll/statuses]
-    (-> (host-with-statuses params)
-        (patchwork/render (page-template)
-                          (environment-names)
-                          params)
-        utils/html-response
-        (cache-headers last-changed))))
-
-(defn- render-all [params]
-  (let [[_ last-changed] @poll/statuses]
-    (-> (host-with-statuses params)
-        (patchwork/render-json (page-template)
-                          (environment-names)
-                          params)
-        j/generate-string
-        utils/json-response
-        (cache-headers last-changed))))
-
-(defn- render-styleguide-entry [params]
-  (utils/html-response (styleguide/render (keywordize-keys params) (styleguide-template))))
-
-(defn- render [{params :params uri :uri}]
-  (if (and config/development?
-           (= "/styleguide" uri))
-    (render-styleguide-entry params)
-    (if (= "/all.json" uri)
-      (render-all params)
-      (render-environments params))))
-
 
 (def ^:private config-help (str/join "\n"
                                      ["To kick off, why don't you create a file hosts.yaml with"
@@ -103,9 +32,4 @@
   (println (config/config-params-as-string)))
 
 (def handler
-  (-> render
-      (utils/wrap-param-lists ["systems" "environments"])
-      params/wrap-params
-      (resource/wrap-resource "public")
-      content-type/wrap-content-type
-      not-modified/wrap-not-modified))
+  (handler/create poll/statuses))
---
+++ src/greenyet/handler.clj
@@ -0,0 +1,84 @@
+(ns greenyet.handler
+  (:require [cheshire.core :as j]
+            [clj-yaml.core :as yaml]
+            [clojure.java.io :as io]
+            [clojure.walk :refer [keywordize-keys]]
+            [greenyet
+             [config :as config]
+             [selection :as selection]
+             [utils :as utils]]
+            [greenyet.view
+             [patchwork :as patchwork]
+             [styleguide :as styleguide]]
+            [ring.middleware
+             [content-type :as content-type]
+             [not-modified :as not-modified]
+             [params :as params]
+             [resource :as resource]]
+            [ring.util
+             [response :refer [header]]
+             [time :refer [format-date]]]))
+
+(defn- page-template []
+  (-> "index.template.html" io/resource slurp))
+
+(defn- styleguide-template []
+  (-> "styleguide.template.html" io/resource slurp))
+
+(defn- environment-names []
+  (-> "environment_names.yaml" io/resource slurp yaml/parse-string))
+
+
+(defn cache-headers [response last-changed]
+  (-> response
+      (header "Last-Modified" (format-date (.toDate last-changed)))
+      (header "Cache-Control" "max-age=0,must-revalidate")))
+
+(defn- selected-host-with-statuses [host-with-statuses params]
+  (let [hide-green (= (get params "hideGreen") "true")]
+    (selection/filter-hosts host-with-statuses
+                            (get params "systems")
+                            (get params "environments")
+                            hide-green)))
+
+(defn- render-environments [params statuses]
+  (let [[host-with-statuses last-changed] @statuses]
+    (-> host-with-statuses
+        (selected-host-with-statuses params)
+        (patchwork/render (page-template)
+                          (environment-names)
+                          params)
+        utils/html-response
+        (cache-headers last-changed))))
+
+(defn- render-all [params statuses]
+  (let [[host-with-statuses last-changed] @statuses]
+    (-> host-with-statuses
+        (selected-host-with-statuses params)
+        (patchwork/render-json (page-template)
+                          (environment-names)
+                          params)
+        j/generate-string
+        utils/json-response
+        (cache-headers last-changed))))
+
+(defn- render-styleguide-entry [params]
+  (utils/html-response (styleguide/render (keywordize-keys params) (styleguide-template))))
+
+(defn- render [{params :params uri :uri} statuses]
+  (if (and config/development?
+           (= "/styleguide" uri))
+    (render-styleguide-entry params)
+    (if (= "/all.json" uri)
+      (render-all params statuses)
+      (render-environments params statuses))))
+
+
+(defn create [statuses]
+  (-> (fn [request]
+        (render request statuses))
+      (utils/wrap-param-lists ["systems" "environments"])
+      params/wrap-params
+      (resource/wrap-resource "public")
+      content-type/wrap-content-type
+      not-modified/wrap-not-modified))
